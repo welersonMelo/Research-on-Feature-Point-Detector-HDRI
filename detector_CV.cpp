c13e3c193a9e9eacdc26006b0683f371bfb98748
welerson <welerson.a.melo@gmail.com>
2018-09-11 13:04:13 -0300

    Teste 6 detector CV

--- detector_CV.cpp
+++ detector_CV.cpp
@@ -182,8 +182,8 @@ Mat coefficienceOfVariationMask(Mat aux, int n){
 	//Testando a gaussiana 5x5 na média
 	
 	//float gerador[] = {0.06136,	0.24477,	0.38774,	0.24477,	0.06136};//  - SD(sigma) = 1.0
-	float gerador[] = {0.122581,	0.233062,	0.288713,	0.233062,	0.122581};// - SD(sigma) = 1.5
-	//float gerador[] = {0.153388,	0.221461,	0.250301,	0.221461,	0.153388};// - SD(sigma) = 2.0
+	//float gerador[] = {0.122581,	0.233062,	0.288713,	0.233062,	0.122581};// - SD(sigma) = 1.5
+	float gerador[] = {0.153388,	0.221461,	0.250301,	0.221461,	0.153388};// - SD(sigma) = 2.0
 	
 	Mat gaussianBox = Mat::zeros(cv::Size(5, 5), CV_32F);
 	Mat gen1 = cv::Mat(1, 5, CV_32F, gerador);
@@ -446,7 +446,7 @@ int main(int, char** argv ){
 	int len = strlen(saida);
 	saida[len] = 'R';saida[len+1] = '.';saida[len+2] = 'j';saida[len+3] = 'p';saida[len+4] = 'g';saida[len+5] = '\0';
 	
-	imwrite(saida, input);
+	//imwrite(saida, input);
 	
 	return 0;
 }
